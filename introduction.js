f6eaf8ff94d6a39ce51c921c6e790b75d0a5a62d
Mahmut Göktuğ <101980878+mahmutgoktug@users.noreply.github.com>
2023-01-16 04:19:28 +0300

    fixed.

--- introduction.js
+++ introduction.js
@@ -6,4 +6,21 @@
     .. % .. kalanı hesaplama
     .. ** .. üslü alma
 */
-console.log('bu yazı introduction.js den geldi!')
+// console.log('bu yazı introduction.js den geldi!')
+
+console.log(typeof 'GoktugCelikkaya')
+console.log(typeof 5)
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
+
+let firstName = 'Goktug',
+    age = 21,
+    isStillStudent = true,
+    secondName = null,
+    howMuchMoney = undefined
+
+const PI = 3.14
+const GRAVITY = 9.81
+
+console.log(firstName, age, isStillStudent, secondName, howMuchMoney)
