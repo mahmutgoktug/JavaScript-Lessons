56f0550ee55faed978782c8f3251dd8fd6ab7faf
Mahmut Göktuğ <101980878+mahmutgoktug@users.noreply.github.com>
2023-01-17 02:07:39 +0300

    init.

---
+++ day-04-boolens.js
@@ -0,0 +1,53 @@
+// && ampersand operator example
+    // true && true -> true
+    // true && false -> false
+    // false && false -> false
+
+// || pipe or operator, example
+    // true || true -> true      
+    // true || false -> true      
+    // false || false -> false
+
+// ! negation examples 
+    // let check = 4 > 3                // true
+    // let check = !(4 > 3)             // false
+    // let isLigthOn = true
+    // let isLightOff = !isLightOn      // false
+    // let isMarried = !false           // true
+
+
+// KOŞULLAR "?"(ise) , ":"(değilse)
+
+    //  let isRaining = true
+    //  isRaining
+    //  ? console.log('You need a rain coat.')
+    //  : console.log('No need for a rain coat')
+    
+    //  let isRaining = false
+    //  isRaining
+    //      ? console.log('You need a rain coat.')
+    //      : console.log('No need for a rain coat')
+
+    //  let number = 5
+    //  number > 0
+    //    ? console.log(`${number} is a positive number`)
+    //    : console.log(`${number} is a negative number`)
+    //  number = -5
+
+    //  number > 0
+    //    ? console.log(`${number} is a positive number`)
+    //    : console.log(`${number} is a negative number`)
+
+
+// Window METHODS
+
+    // let sayi = prompt('Bir Sayi Giriniz', '5')
+    // let result = sayi * 2
+
+    //console.log(result  )
+
+
+    // let isDelete = confirm('Silmek istediğinize emin misiniz?')
+    // console.log(
+    //    isDelete ? 'Silme işlemi başarili' : 'Silme işlemi iptal edildi' 
+    // )
