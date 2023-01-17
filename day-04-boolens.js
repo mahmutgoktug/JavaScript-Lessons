05a97a17633a68fcfc459d37b493b3356dfea539
Mahmut Göktuğ <101980878+mahmutgoktug@users.noreply.github.com>
2023-01-17 16:49:08 +0300

    fixed.

--- day-04-boolens.js
+++ day-04-boolens.js
@@ -51,3 +51,54 @@
     // console.log(
     //    isDelete ? 'Silme işlemi başarili' : 'Silme işlemi iptal edildi' 
     // )
+
+
+// Creating a time object
+
+    // const date = new Date()
+    // console.log(date) // Tue Jan 17 2023 15:44:32 GMT+0300 (GMT+03:00)
+
+    // const now = new Date()  
+    // console.log(now.getMonth()) // 0 because the month is January
+
+
+    const date = new Date()
+
+    let year = date.getFullYear(),
+        month = date.getMonth(),
+        day = date.getDate(),
+        hour = date.getHours(),
+        minute = date.getMinutes(),
+        second = date.getSeconds(),
+        dayNumber = date.getDay()
+
+let months = [
+    'Ocak',
+    'Şubat',
+    'Mart',
+    'Nisan',
+    'Mayıs',
+    'Haziran',
+    'Temmuz',
+    'Ağustos',
+    'Eylül',
+    'Ekim',
+    'Kasım',
+    'Aralık'
+]
+
+let days = [
+    'Pazar',
+    'Pazartesi',
+    'Salı',
+    'Çarşamba',
+    'Perşembe',
+    'Cuma',
+    'Cumartesi'
+]
+
+let humanReadableDate = `${day} ${months[month]} ${year}, ${days[dayNumber]}, ${hour}:${minute}:${second}`
+console.log(humanReadableDate)
+
+// 17 Ocak 2023, Salı, 16:10:00
+    
