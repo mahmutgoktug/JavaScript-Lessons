3acdd40da727acce44ad774efc53afbcc4795b70
Mahmut Göktuğ <101980878+mahmutgoktug@users.noreply.github.com>
2023-01-18 20:48:22 +0300

    init.

--- day-05-diziler.js
+++ day-05-diziler.js
@@ -3,17 +3,48 @@
 // console.log(names)
 
 // let names2 = ['Goktug', 21, 2002, 'Celikkaya']
-// names2[0] = 'Mahmut' 
+// names2[0] = 'Mahmut'
 
 // console.log(names2)
 // console.log('Goktug'.split(''))
 
-let array1 = [1, 2, 3]
-let array2 = [4, 5, 6]
-let array3 = [7, 8, 9]
 
-let result = array1.concat(array2, array3, 10, 11, 12)
-console.log(result)
+// let array1 = [1, 2, 3]
+// let array2 = [4, 5, 6]
+// let array3 = [7, 8, 9]
 
-let names = ['Mahmut', 'Goktug', 'Celikkaya']
-console.log( names.indexOf('Goktug') )
+// let result = array1.concat(array2, array3, 10, 11, 12)
+// console.log(result)
+
+// let names = ['Mahmut', 'Goktug', 'Goktug','Celikkaya']
+// console.log( names.indexOf('Goktug') )
+// console.log( names.lastIndexOf('Goktug') )
+
+let names = ['Mahmut', 'Goktug', 'Goktug2', 'Celikkaya']
+names.pop()
+names.shift()
+
+names.unshift('İlk eleman')
+names.push('Son eleman')
+
+// console.log( 'Goktug'.split('').reverse().join('') )
+
+const numbers = [3, 1, 5, [2, 4, 6], 9]
+console.log(numbers[3][2])
+
+console.log(names)
+
+// names.push('Ejder')
+
+// console.log(names)
+
+// let names2 = 'Goktug'.split('')
+
+// console.log( Array.isArray(names2) )
+// console.log( names.join(', ') )
+
+
+// let currentNames = console.log( names.splice(1, 2, 'Goktug3', 'Goktug4') )
+
+// console.log(currentNames)
+// console.log(names)
