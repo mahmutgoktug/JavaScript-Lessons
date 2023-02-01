de5cec970cd6c05b2bd4c529686d638d1de0bf8f
Mahmut Göktuğ <101980878+mahmutgoktug@users.noreply.github.com>
2023-02-01 16:00:29 +0300

    init.

--- day-11.js
+++ day-11.js
@@ -2,10 +2,12 @@
 // Spread => Dağılma, yayma
 // Rest => Kalanın tamamı
 
+/*
 const numbers = [10, 12, 14]
 
 const [number1, number2] = numbers
 
+ 
 console.log(numbers[0]) // 10
 console.log(number1) // 10
 
@@ -13,3 +15,18 @@ console.log(numbers[1]) // 12
 console.log(number2) // 12
 
 console.log(numbers[2]) 
+
+const stack = [
+    ['HTML', 'CSS', ['JAVASCRIPT', 'REACT', 'SVELTE']],
+    ['PHP', 'MYSQL', 'NODEJS']
+]
+
+// const [frontend, backend] = stack
+const [[firstLanguage, secondLanguage, [firstThirdLanguage]], backend] = stack
+
+console.log(firstThirdLanguage);
+*/
+
+const names = ['goktug', 'halil', 'ejder', 'sahure', 'tugba', 'mahmut'] 
+
+const [myName, myFatherName] = names
