e141f0fb749203d486c728821027a50964ad4888
Mahmut Göktuğ <101980878+mahmutgoktug@users.noreply.github.com>
2023-02-01 14:36:51 +0300

    init.

---
+++ day-11.js
@@ -0,0 +1,15 @@
+// Destructuring => şeklini bozma
+// Spread => Dağılma, yayma
+// Rest => Kalanın tamamı
+
+const numbers = [10, 12, 14]
+
+const [number1, number2] = numbers
+
+console.log(numbers[0]) // 10
+console.log(number1) // 10
+
+console.log(numbers[1]) // 12
+console.log(number2) // 12
+
+console.log(numbers[2]) 
